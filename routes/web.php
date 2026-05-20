101a162624d3c30825d02d8b618b483351c86a49
Tic Tac Toe <namakuaidasafa@gmail.com>
2026-05-20 12:14:33 +0700

    pertemuan 12

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DosenController;
+use App\Http\Controllers\PegawaiDBController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -19,3 +22,14 @@ Route::get('pert5', function () {
 Route::get('dosen', [DosenController::class, 'index']);
 
 Route::get('biodata', [DosenController::class, 'biodata']);
+Route::get('/pegawailama/{nama}', [PegawaiController::class, 'index']);
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+
+//blog
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
+
+//route crud
+Route::get('/pegawai', [PegawaiDBController::class, 'index']);
