614fe413e7445a064a701e21165e448a6ccb7afe
Tic Tac Toe <namakuaidasafa@gmail.com>
2026-06-10 01:27:26 +0700

    Pertemuan 14

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\PegawaiDBController;
 use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\LaptopController;
+use App\Http\Controllers\SiswaController;
+
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -68,3 +71,11 @@ Route::get('laptop/edit/{id}', [LaptopController::class, 'edit']);
 Route::post('laptop/update', [LaptopController::class, 'update']);
 Route::get('laptop/hapus/{id}', [LaptopController::class, 'hapus']);
 Route::get('/laptop/cari', [LaptopController::class, 'cari']);
+
+//route CRUD siswa
+Route::get('/siswa', [SiswaController::class, 'index'])->name('siswa.index');
+Route::get('/siswa/create', [SiswaController::class, 'create'])->name('siswa.create');
+Route::post('/siswa', [SiswaController::class, 'store'])->name('siswa.store');
+Route::get('/siswa/{nrp}/edit', [SiswaController::class, 'edit'])->name('siswa.edit');
+Route::put('/siswa/{nrp}', [SiswaController::class, 'update'])->name('siswa.update');
+Route::delete('/siswa/{nrp}', [SiswaController::class, 'destroy'])->name('siswa.destroy');
